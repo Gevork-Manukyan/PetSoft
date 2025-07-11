23c1f6f97159be6e89518b5353a4c91fb4652048
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-07-11 13:43:16 -0700

    Refactor PetForm component to support editing existing pets; implement default values for form fields based on selected pet data.

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,76 +1,103 @@
-import { usePetContext } from "@/lib/hooks"
-import { Button } from "./ui/button"
-import { Input } from "./ui/input"
-import { Label } from "./ui/label"
-import { Textarea } from "./ui/textarea"
+import { usePetContext } from "@/lib/hooks";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
 
 enum PetFormFields {
-    NAME = "name",
-    OWNER = "ownerName",
-    URL = "imageUrl",
-    AGE = "age",
-    NOTES = "notes",
+  NAME = "name",
+  OWNER = "ownerName",
+  URL = "imageUrl",
+  AGE = "age",
+  NOTES = "notes",
 }
 
 type PetFormProps = {
-    actionType: "add" | "edit";
-    onFormSubmit: () => void;
-}
+  actionType: "add" | "edit";
+  onFormSubmit: () => void;
+};
 
 export default function PetForm({ actionType, onFormSubmit }: PetFormProps) {
-    const { handleAddPet } = usePetContext();
-    
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        
-        const formData = new FormData(e.target as HTMLFormElement);
-        const newPet = {
-            name: formData.get(PetFormFields.NAME) as string,
-            ownerName: formData.get(PetFormFields.OWNER) as string,
-            imageURL: formData.get(PetFormFields.URL) as string || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
-            age: Number(formData.get(PetFormFields.AGE) as string),
-            notes: formData.get(PetFormFields.NOTES) as string,
-        }
-        handleAddPet(newPet);
-        onFormSubmit();
-    }
-    
-    return (
-        <form onSubmit={handleSubmit} className="flex flex-col">
-            <div className="space-y-3">
-                <FormDiv>
-                    <Label htmlFor={PetFormFields.NAME}>Name</Label>
-                    <Input id={PetFormFields.NAME} name={PetFormFields.NAME} type="text" required />
-                </FormDiv>
-                <FormDiv>
-                    <Label htmlFor={PetFormFields.OWNER}>Owner</Label>
-                    <Input id={PetFormFields.OWNER} name={PetFormFields.OWNER} type="text" required />
-                </FormDiv>
-                <FormDiv>
-                    <Label htmlFor={PetFormFields.URL}>Image URL</Label>
-                    <Input id={PetFormFields.URL} name={PetFormFields.URL} type="text" />
-                </FormDiv>
-                <FormDiv>
-                    <Label htmlFor={PetFormFields.AGE}>Age</Label>
-                    <Input id={PetFormFields.AGE} name={PetFormFields.AGE} type="number" required />
-                </FormDiv>
-                <FormDiv>
-                    <Label htmlFor={PetFormFields.NOTES}>Notes</Label>
-                    <Textarea id={PetFormFields.NOTES} name={PetFormFields.NOTES} rows={3} required />
-                </FormDiv>
-            </div>
+  const { handleAddPet, selectedPet } = usePetContext();
 
-            <Button className="mt-5 self-end" type="submit">
-                {actionType === "add" ? "Add a new pet" : "Edit pet"}
-            </Button>
-        </form>
-    )
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const newPet = {
+      name: formData.get(PetFormFields.NAME) as string,
+      ownerName: formData.get(PetFormFields.OWNER) as string,
+      imageURL:
+        (formData.get(PetFormFields.URL) as string) ||
+        "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
+      age: Number(formData.get(PetFormFields.AGE) as string),
+      notes: formData.get(PetFormFields.NOTES) as string,
+    };
+    handleAddPet(newPet);
+    onFormSubmit();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <div className="space-y-3">
+        <FormDiv>
+          <Label htmlFor={PetFormFields.NAME}>Name</Label>
+          <Input
+            id={PetFormFields.NAME}
+            name={PetFormFields.NAME}
+            type="text"
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.name : ""}
+          />
+        </FormDiv>
+        <FormDiv>
+          <Label htmlFor={PetFormFields.OWNER}>Owner</Label>
+          <Input
+            id={PetFormFields.OWNER}
+            name={PetFormFields.OWNER}
+            type="text"
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.ownerName : ""}
+          />
+        </FormDiv>
+        <FormDiv>
+          <Label htmlFor={PetFormFields.URL}>Image URL</Label>
+          <Input
+            id={PetFormFields.URL}
+            name={PetFormFields.URL}
+            type="text"
+            defaultValue={actionType === "edit" ? selectedPet?.imageURL : ""}
+          />
+        </FormDiv>
+        <FormDiv>
+          <Label htmlFor={PetFormFields.AGE}>Age</Label>
+          <Input
+            id={PetFormFields.AGE}
+            name={PetFormFields.AGE}
+            type="number"
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.age : ""}
+          />
+        </FormDiv>
+        <FormDiv>
+          <Label htmlFor={PetFormFields.NOTES}>Notes</Label>
+          <Textarea
+            id={PetFormFields.NOTES}
+            name={PetFormFields.NOTES}
+            rows={3}
+            required
+            defaultValue={actionType === "edit" ? selectedPet?.notes : ""}
+          />
+        </FormDiv>
+      </div>
+
+      <Button className="mt-5 self-end" type="submit">
+        {actionType === "add" ? "Add a new pet" : "Edit pet"}
+      </Button>
+    </form>
+  );
 }
 
 function FormDiv({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="space-y-1">
-            {children}
-        </div>
-    )
+  return <div className="space-y-1">{children}</div>;
 }
